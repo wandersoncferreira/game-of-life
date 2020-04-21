f47a8d24fb35b5cf039a62887dba7a626147a609
Wanderson Ferreira <iagwanderson@gmail.com>
2020-04-21 16:49:35 -0300

    09-test: dead cell with three neighbors should reborn in the next iteration

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -6,10 +6,13 @@
   ([cells]
    {:cells cells}))
 
-(defn make-cell [x y]
-  {:x x
-   :y y
-   :alive? true})
+(defn make-cell
+  ([x y]
+   (make-cell x y true))
+  ([x y alive?]
+   {:x      x
+    :y      y
+    :alive? alive?}))
 
 (defn- abs [v]
   (if (pos? v) v (* -1 v)))
@@ -36,19 +39,41 @@
 (defn make-game [board]
   {:board board})
 
-(defn next-iteration [{:keys [board] :as game}]
-  (let [cells (get-in game [:board :cells])]
-    (make-game
-     (make-board
-      (for [c cells
-            :let [neigh (neighbors board c)
-                  n (count neigh)]]
-        (if (or (= n 2)
-                (= n 3))
-          (assoc c :alive? true)
-          (assoc c :alive? false)))))))
+(defn- create-neighborhood [{:keys [x y]}]
+  (for [i (range (- x 1) (+ x 2))
+        j (range (- y 1) (+ y 2))]
+    (make-cell i j false)))
 
-(defn is-alive? [board cell]
+(defn should-reborn? [board cell]
+  (let [n (neighbors board cell)]
+    (and (false? (:alive? cell))
+         (= 3 (count n)))))
+
+(defn next-iteration [{:keys [board] :as game}]
+  (let [next (loop [cells            (get-in game [:board :cells])
+                    next-iter-cells  []
+                    potencial-reborn []]
+               (if (empty? cells)
+                 {:next-iter-cells  next-iter-cells
+                  :potencial-reborn potencial-reborn}
+                 (let [cell (first cells)
+                       n    (count (neighbors board cell))]
+                   (if (or (= n 2) (= n 3))
+                     (recur (rest cells)
+                            (conj next-iter-cells (assoc cell :alive? true))
+                            (concat potencial-reborn (create-neighborhood cell)))
+                     (recur (rest cells)
+                            (conj next-iter-cells (assoc cell :alive? false))
+                            (concat potencial-reborn (create-neighborhood cell)))))))
+        reborn (->> next
+                    :potencial-reborn
+                    flatten
+                    (filter (fn [r] (should-reborn? board r)))
+                    (map #(assoc % :alive? true)))]
+    (make-game
+     (make-board (concat (:next-iter-cells next) reborn)))))
+
+(defn is-alive? [{:keys [board]} cell]
   (let [{:keys [x y]} cell]
     (:alive?
      (first (filter (fn [c]
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -40,7 +40,7 @@
         board (add-cell board (make-cell 0 0))
         game (make-game board)
         new-game (next-iteration game)]
-    (is (false? (is-alive? (:board new-game) (make-cell 0 0))))))
+    (is (false? (is-alive? new-game (make-cell 0 0))))))
 
 (deftest a-cell-with-two-neighbors-should-live-in-next-iteration
   (let [board (reduce
@@ -50,7 +50,7 @@
                [[0 1] [1 1] [1 0]])
         game (make-game board)
         new-game (next-iteration game)]
-    (is (true? (is-alive? (:board new-game) (make-cell 1 1))))))
+    (is (true? (is-alive? new-game (make-cell 1 1))))))
 
 (deftest a-cell-with-three-neighbors-should-live-in-next-iteration
   (let [board (reduce
@@ -60,7 +60,7 @@
                [[0 1] [1 1] [1 0] [0 0]])
         game (make-game board)
         new-game (next-iteration game)]
-    (is (true? (is-alive? (:board new-game) (make-cell 1 1))))))
+    (is (true? (is-alive? new-game (make-cell 1 1))))))
 
 (deftest a-cell-with-more-than-three-neighbors-should-die-in-next-iteration
   (let [board (reduce
@@ -72,4 +72,14 @@
                 [2 0] [2 1] [2 2]])
         game (make-game board)
         new-game (next-iteration game)]
-    (is (false? (is-alive? (:board new-game) (make-cell 1 1))))))
+    (is (false? (is-alive? new-game (make-cell 1 1))))))
+
+(deftest a-dead-cell-with-three-neighbors-should-reborn-in-next-iteration
+  (let [board (reduce
+               (fn [board [x y]]
+                 (add-cell board (make-cell x y)))
+               (make-board)
+               [[0 1] [1 1] [1 0]])
+        game (make-game board)
+        new-game (next-iteration game)]
+    (is (true? (is-alive? new-game (make-cell 0 0))))))
